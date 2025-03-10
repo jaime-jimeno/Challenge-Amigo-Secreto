f9c7b0bbdc0f8e7bf5be7293ed65fc406d808658
jaime-jimeno <jaime.jimeno.passi@gmail.com>
2025-03-10 11:35:17 -0400

    Primera versión del desafío amigo secreto

---
+++ app.js
@@ -0,0 +1,82 @@
+//Se crea un arreglo vacio para almacenar los nombres de los amigos
+let listaNombres = [];
+
+function agregarAmigo(){
+       let nombre = document.getElementById('amigo').value;
+       let nombreConMayuscula = nombrePropio(nombre);
+
+// Si el campo esta en blanco, mensaje de alerta
+       if(nombre == false){
+        alert('No has ingresado un nombre.');
+        return;
+        } 
+// Si el nombre ya ha sido ingresado, mensaje de alerta
+        else if(listaNombres.includes(nombreConMayuscula)){
+            alert("Este nombre ya ha sido ingresado.");
+            document.getElementById('amigo').focus();
+        }
+// Si el nombre es aceptable, se agrega a la lista 
+// y se imprime en pantalla
+        else{
+            listaNombres.push(nombreConMayuscula);
+            console.log(listaNombres);
+            imprimirListaAmigos();
+            document.getElementById('amigo').focus();
+        }
+    
+    limpiarCaja();
+}
+
+function imprimirListaAmigos(){
+    let listaAmigos = document.getElementById('listaAmigos');
+
+// Evita que se duplique el nombre ya ingresado
+    listaAmigos.innerHTML = '';
+
+//Se recorre el arreglo para imprimir los nombres en pantalla
+    for(let i=0; i < listaNombres.length; i++){
+        listaAmigos.innerHTML += `<li>${listaNombres[i]}<li>`;
+    }
+}
+
+function limpiarCaja() {
+    document.getElementById('amigo').value = '';
+    document.getElementById('amigo').focus;
+}
+
+function sortearAmigo(){
+// Se elige un nombre al azar desde el arreglo
+    let amigoSorteado = listaNombres[Math.floor(Math.random() * listaNombres.length)];
+
+// Mensaje de alerta si hay menos de dos amigos en la lista para sortear    
+    if(listaNombres.length < 2){
+        alert("Debes ingresar al menos dos amigos para realizar el sorteo.");
+        return;
+    }
+
+    document.getElementById('resultado').innerHTML = `Tu amigo sorteado es: ${amigoSorteado}.`;
+
+    //Borra la lista de amigos luego de realizar el sorteo
+    document.querySelector('ul').innerHTML = '';
+
+    console.log(amigoSorteado);
+
+}
+
+agregarAmigo();
+
+//Permitir que puedan agregar amigos con la tecla 'Enter'
+document.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        document.getElementById("botonAñadir").click();
+    }
+});
+
+// Cambiar a mayuscula primera letra y a minuscula el resto de las letras de cada nombre ingresado
+
+function nombrePropio(str) {
+    if (str.length === 0) return str;
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+
